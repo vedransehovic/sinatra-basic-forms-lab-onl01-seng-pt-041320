af6d4c6620d7b9e3b6fcd7df4b35f1db1580ab7e
vedransehovic <vedran.sehovic@gmail.com>
2020-06-27 20:01:19 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,25 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/'  do
+        #code
+        erb :index
+    end
 
+    get '/new' do
+        #code
+
+        erb :create_puppy
+    end
+
+    post '/puppy' do
+        name = params[:name]
+        breed = params[:breed]
+        age = params[:age]
+        @puppy = Puppy.new(name, breed, age)
+        
+        #load view
+        erb :display_puppy
+    end
+    
 end
